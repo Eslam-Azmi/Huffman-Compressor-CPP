44e0a6bd8bfe5021a96113a1af0542a40d23bbbe
Eslam-Azmi <eslamazmi025@gmail.com>
2026-04-12 21:39:47 +0200

    chore: permanently remove junk files and add gitignore

--- tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-uto printTree = [&](auto self, Node* root, int indent) -> void {
-    //     if (root == nullptr) return;
-        
-    //     self(self, root->left, indent + 1);
-    //     cout << string(indent * 4, ' ') << root->freq << endl;
-    //     self(self, root->right, indent + 1);
-    // };
-    // printTree
