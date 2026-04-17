71de2cfb075e1e693b456cae4be7b5b32414e8d3
Eslam-Azmi <eslamazmi025@gmail.com>
2026-04-18 01:13:26 +0200

    feat: implement full decompression engine with node ID stabilization

--- main.cpp
+++ main.cpp
@@ -5,21 +5,27 @@
 
 using namespace std;
 
+void compress();
+void decompress();
+
 struct Node{
     int freq;
     char character;
+    int id;
     Node* left;
     Node* right;
 
-    Node (char character, int frequency){
+    Node (char character, int frequency, int id){
         this->character = character;
         freq = frequency;
+        this->id = id;
         left = right = nullptr;
     }
 
-    Node (int frequency, Node* l, Node* r){
+    Node (int frequency, int id, Node* l, Node* r){
         character = '\0';
         freq = frequency;
+        this->id = id;
         left = l;
         right = r;
     }
@@ -31,7 +37,8 @@ struct Node{
 
 struct Compare {
     bool operator()(Node* a, Node* b){
-        return a->freq > b->freq;  
+        if (a->freq != b->freq) return a->freq > b->freq;
+        return a->id > b->id;  
     }
 };
 
@@ -47,7 +54,41 @@ void generateHuffman(Node* root, string bits, unordered_map<char, string>& Huffm
     generateHuffman(root->right, bits + "1", Huffman);
 }
 
+Node* buildTree(unordered_map<char, int>& counts){
+    priority_queue<Node*, vector<Node*>, Compare> pq; 
+    int nodeID = 1000;
+
+    for (auto& pair : counts){
+        Node* n = new Node(pair.first, pair.second, (int)(unsigned char)pair.first);
+        pq.push(n);
+    }
+
+    while (pq.size() > 1){
+        Node* left = pq.top(); pq.pop();
+        Node* right = pq.top(); pq.pop();
+
+        Node* parent = new Node(left->freq + right->freq, nodeID++, left, right);
+        pq.push(parent);
+    }
+    return pq.top();
+}
+
 int main(){
+    cout << "1. Compress \n2. Decompress\n Choice: ";
+    int choice;
+    cin >> choice;
+    cin.ignore();
+
+    if (choice == 1){
+        compress();
+    }else if (choice == 2){
+        decompress();
+    }
+
+    return 0; 
+}
+
+void compress(){
     // Reading the file and counting the frequency for each character
     string filename;
 
@@ -55,11 +96,11 @@ int main(){
     getline(cin, filename);
 
     ifstream inFile;
-    inFile.open(filename);
+    inFile.open(filename, ios::binary);
 
     if (inFile.fail()){
         cout << "No exist for this file!" << endl;
-        return 1; 
+        return; 
     }
 
     char c;
@@ -78,21 +119,8 @@ int main(){
         cout << display << " : " << pair.second << endl;
     }
     // Nodes for every unique character and putting them in priority queue(min)
-    priority_queue<Node*, vector<Node*>, Compare> pq; 
-
-    for (auto& pair : counts){
-        Node* n = new Node(pair.first, pair.second);
-        pq.push(n);
-    }
-
-    Node* root;
-    while (pq.size() > 1){
-        Node* left = pq.top(); pq.pop();
-        Node* right = pq.top(); pq.pop();
-        Node* parent = new Node(left->freq + right->freq, left, right);
-        pq.push(parent);
-    }
-    root = pq.top();
+    
+    Node* root = buildTree(counts);
 
     // auto printTree = [&](auto self, Node* root, int indent) -> void {
     //     if (root == nullptr) return;
@@ -130,7 +158,7 @@ int main(){
 
     ofstream outFile("compressed.huff", ios::binary);
 
-    // Metadata for the decompressor
+    // Metadata for the decompressor (unique characters - frequincy for each char - length of bits)
     int mapSize = counts.size();
     outFile.write(reinterpret_cast<const char*>(&mapSize), sizeof(mapSize));
 
@@ -160,6 +188,47 @@ int main(){
         byte <<= (8 - bitCounter);
         outFile.put(byte);
     }
+}
 
-    return 0; 
+void decompress(){
+    ifstream inFile("compressed.huff", ios::binary);
+    
+    int mapSize; 
+    inFile.read(reinterpret_cast<char*>(&mapSize), sizeof(mapSize));
+
+    unordered_map<char, int> counts;
+    for (int i = 0; i < mapSize; i++){
+        char c = inFile.get();
+        int freq;
+
+        inFile.read(reinterpret_cast<char*>(&freq), sizeof(freq));
+        counts[c] = freq;
+    }
+
+    int totalBits;
+    inFile.read(reinterpret_cast<char*>(&totalBits), sizeof(totalBits));
+    
+    Node* root = buildTree(counts);
+
+    ofstream outFile("decompressed.txt", ios::binary);
+
+    Node* current = root; 
+    int bitRead = 0;
+    unsigned char byte;
+
+    while (inFile.read(reinterpret_cast<char*>(&byte), 1)){
+        for (int i = 7; i >= 0; i--){
+            if (bitRead >= totalBits) break;
+            if (((byte >> i) & 1) == 1){
+                current = current->right;
+            }else{
+                current = current->left;
+            }
+            if (current->isLeaf()){
+                outFile.put(current->character);
+                current = root;
+            }
+            bitRead++;
+        }
+    }
 }
